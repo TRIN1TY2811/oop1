9d838d0b9a4bfd63d5c960516c886124530b894d
TRIN1TY2811 <jamessearll28@gmail.com>
2026-06-11 06:17:50 +0800

    new

--- SchoolSystem3/src/main/java/schoolsystem3/SchoolSystem3.java
+++ SchoolSystem3/src/main/java/schoolsystem3/SchoolSystem3.java
@@ -2,8 +2,6 @@ package schoolsystem3;
 
 public class SchoolSystem3 {
 
-   
-     
     public static void main(String[] args) {
         Homepage hm = new Homepage();
         hm.setVisible(true);
